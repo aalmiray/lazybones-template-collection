3fb054779a7cdd25e4a7fe8724a52e656cb52d6f
aalmiray <aalmiray@gmail.com>
2015-03-05 21:07:31 +0100

    Upgrade afterburnerfx template

--- templates/afterburnerfx/lazybones.groovy
+++ templates/afterburnerfx/lazybones.groovy
@@ -6,7 +6,7 @@ props.project_capitalized_name = props.project_name.capitalize()
 props.project_group_id = ask("Define value for 'group' [org.example]: ", "org.example", "group")
 props.project_version = ask("Define value for 'version' [0.1.0-SNAPSHOT]: ", "0.1.0-SNAPSHOT", "version")
 props.project_package_name = ask("Define value for 'packageName' ["+ props.project_group_id +"]: ", props.project_group_id, "packageName")
-props.afterburner_version = ask("Define value for 'afterburnerVersion' [1.5.1]: ", "1.5.1", "afterburnerVersion")
+props.afterburner_version = ask("Define value for 'afterburnerVersion' [1.6.2]: ", "1.6.2", "afterburnerVersion")
 
 processTemplates 'build.gradle', props
 processTemplates 'gradle.properties', props
